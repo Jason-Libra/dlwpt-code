78b6467d3248831bc39458eeebb1b6f206d732da
Libra <1792307595@qq.com>
2024-06-20 01:40:37 +0800

    up to ch4 p4

---
+++ test.py
@@ -0,0 +1,11 @@
+var: str = "Hello, World!"
+
+print(f"{var}")
+print(f"{var:5}")
+print(f"{var:20}")
+print(f"{var:>20}")
+print(f"{var:<20}")
+print(f"{var:^20}")
+print(f"{var:_^20}")
+print(f"{var:*<20}")
+print(f"{var:#>20}")

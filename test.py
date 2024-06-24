d12f6d7a832562007e8e390334f92e5accdb33b0
Libra <1792307595@qq.com>
2024-06-24 10:58:18 +0800

    up to ch8

--- test.py
+++ test.py
@@ -9,3 +9,10 @@ print(f"{var:^20}")
 print(f"{var:_^20}")
 print(f"{var:*<20}")
 print(f"{var:#>20}")
+
+
+def sum(a, b):
+    for i in range(32):
+        if i == 12:
+            print(f"{a} + {b} = {a + b}")
+    return a + b
